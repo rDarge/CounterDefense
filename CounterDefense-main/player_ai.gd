c622040146f9359d4ffe6c2a690d22160e51139d
SamLS <SamLS@users.noreply.github.com>
2022-04-02 18:53:00 -0400

    Add files via upload

--- CounterDefense-main/player_ai.gd
+++ CounterDefense-main/player_ai.gd
@@ -1,10 +1,12 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var velocity = Vector2()
+var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,12 +15,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	velocity = move_and_slide(velocity)
+
+func _input(event):
+	print(event)
+	velocity = Vector2() # The player's movement vector.
+	if event.is_action("ui_right"):
+		velocity.x += 100
+	if event.is_action("ui_left"):
+		velocity.x -= 100
+	if event.is_action("ui_down"):
+		velocity.y += 100
+	if event.is_action("ui_up"):
+		velocity.y -= 100
