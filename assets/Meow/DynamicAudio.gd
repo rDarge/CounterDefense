a481b6470469c3094aeadae800bada4701c03cfe
KoolKat69 <48398513+KoolKat69@users.noreply.github.com>
2022-04-05 10:52:24 -0400

    Cleanup
    Removed animations not being used
    cleaned up code not being used anymore.

--- assets/Meow/DynamicAudio.gd
+++ assets/Meow/DynamicAudio.gd
@@ -44,52 +44,6 @@ func dir_counter2(path):
 	if debug: print("dircounter return : " + str(array.size()))
 	return array
 
-#dir_counter2 should depreciate this
-func dir_counter(path):
-	var array = []
-	var omit = ".import"
-	var i = 0
-	var dir = Directory.new()
-	if dir.open(path) == OK:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				if debug: print("Found directory: " + file_name)
-			elif file_name.right(file_name.length()-omit.length()) != omit:
-				i = i + 1
-				array.append(file_name)
-				if debug: print("Found file: " + file_name)
-			file_name = dir.get_next()
-	else:
-		print("An error occurred when trying to access the path. Returning : 0")
-		return 0
-	if debug: print("dircounter return : " + str(i))
-	for j in array:
-		print("array: " + j)
-	return i
-
-#dir_counter2 should depreciate this
-func get_audio(path, i):
-	var omit = ".import"
-	var x = 0
-	var dir = Directory.new()
-	if dir.open(path) == OK:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				if debug: print("Found directory: " + file_name)
-			elif file_name.right(file_name.length()-omit.length()) != omit:
-				x = x + 1
-				if debug: print("Found file: " + file_name)
-				if (x == i):
-					if debug: print("Returning " + file_name)
-					return file_name
-			file_name = dir.get_next()
-	else:
-		print("An error occurred when trying to access the path.")
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
