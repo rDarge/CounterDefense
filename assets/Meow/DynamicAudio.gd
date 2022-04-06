126a642814633d6787a6cb1688eadfc55b854a61
KoolKat69 <48398513+KoolKat69@users.noreply.github.com>
2022-04-06 18:04:51 -0400

    Fixed Music
    This now uses the import files to locate the resources to load them. This will now work compiled in EXE.

--- assets/Meow/DynamicAudio.gd
+++ assets/Meow/DynamicAudio.gd
@@ -5,17 +5,30 @@ var musicOn = true
 var masterVol = -10
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var dirMusic = "res://Meow/Music/Reg/"
-	#dir_contents(dirMusic)
+	var dirMusic = "res://Meow/Music/Reg"
 	var player = AudioStreamPlayer.new()
 	self.add_child(player)
 	player.volume_db = masterVol
-	var playlist = dir_counter2(dirMusic)
+	var playlistFiles = dir_counter(dirMusic,".import")
+	var playlistPaths = assetsPaths(playlistFiles,"path=\"")
 	while (musicOn):
-		#player.stream = load(dirMusic+"/"+get_audio(dirMusic,random(dir_counter(dirMusic))))
-		player.stream = load(dirMusic+"/"+playlist[random(playlist.size())-1])
+		player.stream = load(playlistPaths[random(playlistPaths.size())-1])
 		player.play()
 		yield(player, "finished")
+		
+func assetsPaths(imports,find):
+	var file = File.new()
+	var array = []
+	for i in imports:
+		if debug: print("i="+i)
+		file.open(i,File.READ)
+		while (file.is_open()):
+			var line = file.get_line()
+			if line.left(find.length()) == find:
+				if debug: print("found:" + line.substr(find.length(),line.length()-find.length()-1))
+				array.append(line.substr(find.length(),line.length()-find.length()-1))
+				file.close()
+	return array
 
 func random(count):
 	var randSeed = RandomNumberGenerator.new()
@@ -24,9 +37,8 @@ func random(count):
 	if debug: print("randNum = " + str(randNum))
 	return randNum
 
-func dir_counter2(path):
+func dir_counter(path,ext):
 	var array = []
-	var omit = ".import"
 	var dir = Directory.new()
 	if dir.open(path) == OK:
 		dir.list_dir_begin()
@@ -34,8 +46,8 @@ func dir_counter2(path):
 		while file_name != "":
 			if dir.current_is_dir():
 				if debug: print("Found directory: " + file_name)
-			elif file_name.right(file_name.length()-omit.length()) != omit:
-				array.append(file_name)
+			elif file_name.right(file_name.length()-ext.length()) == ext:
+				array.append(path+"/"+file_name)
 				if debug: print("Found file: " + file_name)
 			file_name = dir.get_next()
 	else:
